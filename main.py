2eaeca2c115a7cd87f03428c42abccc342908251
pyLerner <lerner.v.b@gmail.com>
2023-05-18 18:04:21 +0300

    Error bad file exception

--- main.py
+++ main.py
@@ -14,65 +14,76 @@ log_file = st.file_uploader(
 
 if not log_file is None:
 
-    # Получение массива данных  NMEA из лога
-    nmea_track = extract_nmea_from_log(log_file)
+    try:
+        # Получение массива данных  NMEA из лога
+        nmea_track = extract_nmea_from_log(log_file)
 
-    # Преобразование  времени в datetime
-    track_time = track_time_to_datetime(nmea_track[:, 0])
 
-    # Пересчитываем координаты в десятичные градусы
-    track = nmea_coordinates_to_degrees(nmea_track)[:, 1:3]
 
-    # Приведение к виду для загрузки в leaflet/foliant:
-    track2list = track.tolist()
+        # Преобразование  времени в datetime
+        track_time = track_time_to_datetime(nmea_track[:, 0])
 
-    # Инициализация карты с центром в начальной точке трека
-    # my_map = folium.Map(
-    #     location=track2list[0],
-    #     zoom_start=12
-    # )
+        # Пересчитываем координаты в десятичные градусы
+        track = nmea_coordinates_to_degrees(nmea_track)[:, 1:3]
 
-    # Среднее арифметическая координата всех точек трека
-    # mean_track_dot = track.mean(axis=0).tolist()
-    # Центрирование карты в точке среднеарифметического значений трека
-    # my_map = folium.Map(
-    #     location=mean_track_dot,
-    #     zoom_start=12
-    # )
+        # Приведение к виду для загрузки в leaflet/foliant:
+        track2list = track.tolist()
 
-    # Параметры баундингбокса
-    bottom_right = [
-        track.max(axis=0)[0],
-        track.min(axis=0)[1]
-    ]
+        # Инициализация карты с центром в начальной точке трека
+        # my_map = folium.Map(
+        #     location=track2list[0],
+        #     zoom_start=12
+        # )
 
-    top_left = [
-        track.min(axis=0)[0],
-        track.max(axis=0)[1]
-    ]
+        # Среднее арифметическая координата всех точек трека
+        # mean_track_dot = track.mean(axis=0).tolist()
+        # Центрирование карты в точке среднеарифметического значений трека
+        # my_map = folium.Map(
+        #     location=mean_track_dot,
+        #     zoom_start=12
+        # )
 
-    # Центр баундингбокса
-    center_dot = (np.array(bottom_right) + np.array(top_left)) / 2
-    center_dot = center_dot.tolist()
+        # Параметры баундингбокса
+        bottom_right = [
+            track.max(axis=0)[0],
+            track.min(axis=0)[1]
+        ]
 
-    # Центрирование карты в центре баундингбокса
-    my_map = folium.Map(location=center_dot)
+        top_left = [
+            track.min(axis=0)[0],
+            track.max(axis=0)[1]
+        ]
 
-    # Отрисовка трека точками
-    draw_dot_track_to_map(my_map, track_time, track2list)
+        # Центр баундингбокса
+        center_dot = (np.array(bottom_right) + np.array(top_left)) / 2
+        center_dot = center_dot.tolist()
 
-    # Трек линиями
-    # draw_polyline_trip_track(my_map, track2list)
+        # Центрирование карты в центре баундингбокса
+        my_map = folium.Map(location=center_dot)
 
-    # Автомасштаб по границам баундингбокса
-    my_map.fit_bounds(
-        [bottom_right, top_left],
-        padding=top_left  # Отступы по краям (не понятно в каких единицах)
-    )
+        # Отрисовка трека точками
+        draw_dot_track_to_map(my_map, track_time, track2list)
 
-    st_data = streamlit_folium.st_folium(
-        my_map, width=725
-    )
+        # Трек линиями
+        # draw_polyline_trip_track(my_map, track2list)
+
+        # Автомасштаб по границам баундингбокса
+        my_map.fit_bounds(
+            [bottom_right, top_left],
+            padding=top_left  # Отступы по краям (не понятно в каких единицах)
+        )
+
+        st_data = streamlit_folium.st_folium(
+            my_map, width=725
+        )
+
+
+    except KeyError as e:
+        st.error(
+            """
+            ** Ошибка чтения файла. Попробуйте другой файл **
+            """
+        )
     #
     # # Генерируем HTML страницу
     # file = 'map.html'
